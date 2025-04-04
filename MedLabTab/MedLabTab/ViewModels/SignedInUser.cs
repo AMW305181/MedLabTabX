338d4de94643447be94c73e4af16ac1780636b91
AMW305181 <aw305181@student.polsl.pl>
2025-04-04 18:52:08 +0200

    bare min prep

---
+++ MedLabTab/MedLabTab/ViewModels/SignedInUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedLabTab.DatabaseModels;
+
+namespace MedLabTab.ViewModels
+{
+    //to jest klasa tymczasowa, by katalog byl utworzony w mainie
+    internal class SignedInUser : User
+    {
+        public SignedInUser(User user) 
+        {
+            this.id=user.id;
+            this.UserType = user.UserType;
+            this.Name = user.Name;
+            this.Surname = user.Surname;
+            this.Password = user.Password;
+            this.PESEL=user.PESEL;
+            this.PhoneNumber = user.PhoneNumber;
+        }
+    }
+}
