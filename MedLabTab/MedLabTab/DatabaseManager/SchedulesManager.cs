c85c347e02fa0735d89ba2ca69ce232c28d59fbe
AMW305181 <aw305181@student.polsl.pl>
2025-05-03 16:42:51 +0200

    dbmanager clean

---
+++ MedLabTab/MedLabTab/DatabaseManager/SchedulesManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedLabTab.DatabaseModels;
+
+namespace MedLabTab.DatabaseManager
+{
+    internal class SchedulesManager
+    {
+        public Schedule GetSchedule(MedLabContext db,int Id)
+        {
+            try
+            {
+                var schedule = db.Schedules.Where(s => s.id == Id).FirstOrDefault();
+                if (schedule != null) { return schedule; }
+                return null;
+            }
+            catch { return null; }
+        }
+        public List<Schedule> GetAllDates(MedLabContext db)
+        {
+            try
+            {
+                List<Schedule> AvailableDates = db.Schedules.ToList();
+                return AvailableDates;
+            }
+            catch { return null; }
+        }
+    }
+}
---
+++ MedLabTab/MedLabTab/DatabaseManager/TestHistoryManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MedLabTab.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedLabTab.DatabaseManager
+{
+    internal class TestHistoryManager
+    {
+        public bool AddTestHistory(MedLabContext db, TestHistory newTest)
+        {
+            try
+            {
+                db.TestHistories.Add(newTest);
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+        public bool RemoveTestHistory(MedLabContext db,int visitId)
+        {
+            try
+            {
+                db.TestHistories.RemoveRange(db.TestHistories.Where(t => t.VisitId == visitId));
+                db.SaveChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+        public List<TestHistory> GetTestsInVisit(MedLabContext db, int visitId)
+        {
+            try
+            {
+                List<TestHistory> TestsInVisit = db.TestHistories.Where(t => t.VisitId == visitId).ToList();
+                if (TestsInVisit != null) { return TestsInVisit; }
+                return null;
+            }
+            catch { return null; }
+        }
+        public List<TestHistory> GetCompletedTests(MedLabContext db)
+        {
+                return db.TestHistories
+                    .Include(th => th.Test)
+                    .Include(th => th.Visit)
+                        .ThenInclude(v => v.TimeSlot)
+                            .ThenInclude(ts => ts.Nurse)
+                    .Include(th => th.Patient)
+                    .Include(th => th.Analyst)
+                    .Where(th => th.Visit.IsActive == false)
+                    .OrderByDescending(th => th.Visit.TimeSlot.Date)
+                    .ThenByDescending(th => th.Visit.TimeSlot.Time)
+                    .ToList();
+
+        }
+    }
+}
